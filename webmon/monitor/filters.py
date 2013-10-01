2a0db566393c0a1b5a2fbc2b4062e1efa5f72079
Mikhail Priver <m.priver@gmail.com>
2013-10-02 01:09:48 +0400

    Add styles for filters

--- webmon/monitor/filters.py
+++ webmon/monitor/filters.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 import django_filters
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from .models import CallDataRecord
 
 
 class CallDataRecordFilter(django_filters.FilterSet):
-    start_from = django_filters.DateFilter(name='start', lookup_type='gt')
-    start_to = django_filters.DateFilter(name='start', lookup_type='lt')
-    destination = django_filters.CharFilter(lookup_type='contains')
-    disposition = django_filters.MultipleChoiceFilter(choices=CallDataRecord.DISPOSITION, widget=forms.CheckboxSelectMultiple)
+    start_from = django_filters.DateFilter(name='start', lookup_type='gt', label=_('From'))
+    start_to = django_filters.DateFilter(name='start', lookup_type='lt', label=_('To'))
+    destination = django_filters.CharFilter(lookup_type='contains', label=_('Destination'))
+    disposition = django_filters.MultipleChoiceFilter(choices=CallDataRecord.DISPOSITION,
+                                                      widget=forms.CheckboxSelectMultiple)
     class Meta:
         model = CallDataRecord
         fields = ['start_from', 'start_to', 'source', 'destination', 'disposition']
